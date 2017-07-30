9dc3cd428e285c00485e60459b4c64f5d9e10bb6
Amlan Saikia <amlan.random@gmail.com>
2017-07-30 09:47:04 +0530

    web connector updated

--- functions/connect.php
+++ functions/connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "userlight";
-$password = "lightspeed123";
-$dbname = "mydb_datahub";
+$username = "";
+$password = "";
+$dbname = "";
 
 // Create connection
 $dbcon = new mysqli($servername, $username, $password, $dbname);
